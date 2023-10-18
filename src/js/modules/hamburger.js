0407c8dd314caa07e0d171dc7d0ae06df5670a86
sava911 <pesss929@gmail.com>
2023-10-18 22:07:14 +0300

    new -ver

--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -27,4 +27,14 @@ export default function hamburger(second) {
     });
   });
 
+
+  window.addEventListener('scroll', function () {
+    const scrollPosition = window.scrollY;
+    if (scrollPosition >= 200) {
+      document.querySelector(".header-fixed").classList.add("with-effect");
+    } else {
+      document.querySelector(".header-fixed").classList.remove("with-effect");
+    }
+  });
+
 }
